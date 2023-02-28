8b504dac8170fc9d527e5b3e7fe978a3df62156c
Owlynn <contact@jessalynn.fr>
2023-02-28 18:07:58 +0100

    // POST /api/ideas

--- server/ideas.js
+++ server/ideas.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const ideasRouter = express.Router();
+const { 
+    addToDatabase,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+  } = require('./db');
 
 // GET /api/ideas to get an array of all ideas.
+ideasRouter.get('/', (req,res) => {
+    res.send(getAllFromDatabase('ideas'));
+})
+
 // POST /api/ideas to create a new idea and save it to the database.
+ideasRouter.post('/:ideaId',(req,res) => {
+    const newIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
+})
+
 // GET /api/ideas/:ideaId to get a single idea by id.
 // PUT /api/ideas/:ideaId to update a single idea by id.
 // DELETE /api/ideas/:ideaId to delete a single idea by id.
